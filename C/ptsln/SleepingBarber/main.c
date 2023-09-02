cba8b24d6875b455433daa6331da38a5611a1aad
Shigehiro Fukase <shigehiro.fukase@gmail.com>
2023-09-02 23:19:17 +0900

    [playground-examples-C] SOLID-IDEプロジェクト作成

---
+++ C/ptsln/SleepingBarber/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <netlogger.h>
+
+int lf_reactor_c_main(int argc, const char *argv[]);
+
+void slo_main()
+{
+    int argc = 0;
+    const char *argv[] = {0};
+#ifdef USE_NETLOGGER
+    nl_init();
+#endif
+    lf_reactor_c_main(argc, argv);
+    exit(0);
+}
